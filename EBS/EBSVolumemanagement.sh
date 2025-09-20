1d41dda47f1efd6a032399e20043d69f406fd293
Vishnu Prasad Gettam <vishnugettam@gmail.com>
2025-09-20 12:22:13 +0530

    EBS scripts

---
+++ EBS/EBSVolumemanagement.sh
@@ -0,0 +1,11 @@
+#list information about block devices (like hard drives, SSDs, NVMe, partitions, USB drives).
+
+lsblk 
+
+#report file system disk space usage.
+
+df 
+
+#report file system disk space usage in human readable format.
+
+df -h
